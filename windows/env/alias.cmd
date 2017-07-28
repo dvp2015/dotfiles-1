6add8a647a14e105c173aa2737cabc86d5ca3409
dvp2015 <dmitri_portnov@yahoo.com>
2017-07-28 17:21:39 +0300

    Fixing windows environment files

--- windows/env/alias.cmd
+++ windows/env/alias.cmd
@@ -1,27 +1,31 @@
 :: Aliases
 
-DOSKEY home=cd %USERPROFILE%
-DOSKEY ls=dir /B $*
-DOSKEY ll=dir $*
-DOSKEY rm=del $*
-DOSKEY ed="%ProgramFiles%\Notepad++\notepad++.exe" $*
-DOSKEY ee="%ProgramFiles%\Notepad++\notepad++.exe" %USERPROFILE%\OneDrive\env\env.cmd $*
-DOSKEY ea="%ProgramFiles%\Notepad++\notepad++.exe" %USERPROFILE%\OneDrive\env\alias.cmd $*
-DOSKEY pycharm="%ProgramFiles%\JetBrains\PyCharm Community Edition 2017.1.3\bin\pycharm64.exe" $*
+doskey alias=doskey /macros
+doskey home=cd %USERPROFILE%
+doskey ls=dir /B/D $*
+doskey la=dir /N/W $*
+doskey ll=dir /N $*
+doskey rm=del $*
+doskey ed="%ProgramFiles%\Notepad++\notepad++.exe" $*
+doskey ee="%ProgramFiles%\Notepad++\notepad++.exe" "%USERPROFILE%\dotfiles\windows\env\env.cmd" $*
+doskey ea="%ProgramFiles%\Notepad++\notepad++.exe" "%USERPROFILE%\dotfiles\windows\env\alias.cmd" $*
+doskey ele="%ProgramFiles%\Notepad++\notepad++.exe" "%USERPROFILE%\OneDrive\env\env.cmd" $*
+doskey ela="%ProgramFiles%\Notepad++\notepad++.exe" "%USERPROFILE%\OneDrive\env\alias.cmd" $*
+doskey pycharm="%ProgramFiles%\JetBrains\PyCharm Community Edition 2017.1.3\bin\pycharm64.exe" $*
 
 :: Common directories
 
-DOSKEY od=cd "%USERPROFILE%\OneDrive\$*"
+doskey od=cd "%USERPROFILE%\OneDrive\$*"
 :: Russian is not supported, so using short DOS name for "Google Диск"
-DOSKEY gd=cd "%USERPROFILE%\GOOGLE~1\$*"
-DOSKEY dev=cd "\dev\$*"
-DOSKEY doc="C:\Program Files\Microsoft Office\Office15\WINWORD.exe" $*
-DOSKEY xls="C:\Program Files\Microsoft Office\Office15\EXCEL.exe" $*
-DOSKEY ppt="C:\Program Files\Microsoft Office\Office15\POWERPNT.exe" $*
+doskey gd=cd "%USERPROFILE%\GOOGLE~1\$*"
+doskey dev=cd "\dev\$*"
+doskey doc="C:\Program Files\Microsoft Office\Office15\WINWORD.exe" $*
+doskey xls="C:\Program Files\Microsoft Office\Office15\EXCEL.exe" $*
+doskey ppt="C:\Program Files\Microsoft Office\Office15\POWERPNT.exe" $*
 
 :: Open git bash console
-DOSKEY gb="C:\tools\git\git-bash.exe" $*
+doskey gb="C:\tools\git\gb.cmd" $*
 
 
-::DOSKEY dropbox=cd "%USERPROFILE%\Dropbox\$*"
-::DOSKEY research=cd %USERPROFILE%\Dropbox\Research\
+::doskey dropbox=cd "%USERPROFILE%\Dropbox\$*"
+::doskey research=cd %USERPROFILE%\Dropbox\Research\
