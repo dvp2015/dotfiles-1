b3cb11e55e91a7edf46bc65eb6fc581de64012e2
dvp <dmitri_portnov@yahoo.com>
2021-05-24 13:55:53 +0300

    starutp.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -13,7 +13,22 @@ atreplinit() do repl
     catch
         @warn "OhMyREPL package is not available, add it with Pkg"
     end
-end
-using Random
 
-ENV["JULIA_EDITOR"] = "vim"
+   ( "JULIA_EDITOR" in keys(ENV) ) || Sys.isexecutable("vim") && ( ENV["JULIA_EDITOR"] = "vim" )
+ 
+end
+
+# TODO dvp: move the following to a special package to provide some utilities both in REPL and scripts
+
+mdcd(path::AbstractString; mode::Unsigned = 0o777) = cd(mkpath(path; mode))
+
+
+# TODO dvp: move the following to a special package to customize my REPL only (in interactive mode)
+
+function editstartup()
+     home = ENV[Sys.iswindows() ? "USERPROFILE" : "HOME"]
+     startup = joinpath(home, ".julia", "config", "startup.jl")
+     edit(startup)
+end
+
+
