a44fc1c0adb1eb9ff016c3baa2c351c492131fc9
dvp <dmitri_portnov@yahoo.com>
2021-05-26 11:08:07 +0300

    comment_startup.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -20,11 +20,23 @@ end
 
 # TODO dvp: move the following to a special package to provide some utilities both in REPL and scripts
 
-mdcd(path::AbstractString; mode::Unsigned = 0o777) = cd(mkpath(path; mode))
+"""
+    mdcd(dir)
+
+Make directory `dir` and change to it.
+
+"""
+mdcd(path::AbstractString; mode::Unsigned = 0o777) = mkpath(path; mode) |> cd
 
 
 # TODO dvp: move the following to a special package to customize my REPL only (in interactive mode)
 
+"""
+    editstartup()
+
+Open user's startup.jl in default editor.
+
+"""
 function editstartup()
      home = ENV[Sys.iswindows() ? "USERPROFILE" : "HOME"]
      startup = joinpath(home, ".julia", "config", "startup.jl")
