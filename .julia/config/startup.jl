df947782aac62165f0f0b8611ab26e936885a2ed
dvp <dmitri_portnov@yahoo.com>
2021-08-05 18:32:10 +0300

    fix: julia starup.jl - add TerminalLoggers, fispact 5.0 environment script

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,14 +1,46 @@
-using OhMyREPL
-colorscheme!("OneDark")
-OhMyREPL.enable_autocomplete_brackets(true)
-OhMyREPL.Passes.RainbowBrackets.activate_256colors()
-OhMyREPL.input_prompt!("👠> ")
+atreplinit() do repl
 
-if "JULIA_EDITOR" ∉ keys(ENV) && Sys.isexecutable("vim")
-    ENV["JULIA_EDITOR"] = "vim"
+    try
+        @eval begin
+            using OhMyREPL
+            colorscheme!("OneDark")
+            OhMyREPL.enable_autocomplete_brackets(true)
+            OhMyREPL.Passes.RainbowBrackets.activate_256colors()
+            OhMyREPL.input_prompt!("👠> ")
+        end
+    catch e
+        display("Cannot use OhMyREPL: $e.msg")
+    end
+
+    try
+        @eval begin
+            using Revise
+        end
+    catch e
+        display("Cannot use Revise: $e.msg")
+    end
+
+    try
+        @eval begin
+            using Logging: global_logger
+            using TerminalLoggers: TerminalLogger
+            global_logger(TerminalLogger())
+        end
+    catch e
+        display("Cannot use TerminalLoggers: $e.msg")
+    end
+
+    @eval begin
+        if "JULIA_EDITOR" ∉ keys(ENV) && Sys.isexecutable("vim")
+            ENV["JULIA_EDITOR"] = "vim"
+        end
+
+        myrepl() = joinpath(homedir(), ".myrepl.jl")
+        imyrepl() = include(myrepl())
+        emyrepl() = edit(myrepl())
+    end
+
 end
 
-myrepl() = joinpath(homedir(), ".myrepl.jl")
-imyrepl() = include(myrepl())
-emyrepl() = edit(myrepl())
+
 
