16f329e9e080a67188f0a409f7139f8c098dae72
dvp <d.portnov@iterrf.ru>
2025-06-03 20:34:29 +0300

    zplug remove

--- .config/zsh/custom/alias.zsh
+++ .config/zsh/custom/alias.zsh
@@ -1,7 +1,6 @@
 # convenience aliases for editing ZSH configs
 alias eza='vim ~/.config/zsh/custom/alias.zsh'
 alias ez='vim ~/.zshrc'
-alias eomz="vim ~/.zplug/repos/robbyrussell/oh-my-zsh/oh-my-zsh.sh"
 
 
 
